815260f1674853cc108913700633b9de8c0ea5c4
Louis Varley <louis.varley@landscapeinstitute.org>
2020-02-21 14:35:10 +0000

    updated to include profile image

--- app/views/privacy-policy.php
+++ app/views/privacy-policy.php
@@ -27,16 +27,17 @@ class privacyPolicy extends \myReef\views\view{
 							<p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
 							<p>You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
 							<p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
-							<p>This policy is effective as of 10 February 2020.</p>
+							<p>This policy is effective as of <b>10 February 2020.</b></p>
 							
 							<ul class="a">
 							   <li>We retain the following information about users of our site:
 								   <ul class="i">
 									   <li><b>IP Address</b> which is used to keep track of the user posting a listing and for purposes of IP banning.</li>
-									   <li><b>Email Address</b> Saved against your listing and assists in allowing potential buyers to reach out to you.</li>
-									   <li><b>App user ID</b> which is a unique number assigned to you by facebook for our app.</li>
-									   <li><b>Your Name</b> which is used to pre-fill the name field on listing forms.</li>
-									   <li><b>Contact Link</b> You will provide a contact link, likley in the form a facebook profile link but maybe a mobile number or email address. These are not used by this website other than being displayed on your listing</li>									   
+									   <li><b>Email Address</b> Saved against your listing and allows us to prefill the contact box to assists in allowing potential buyers to reach out to you.</li>
+									   <li><b>App user ID</b> which is a unique number assigned to you by facebook for our app but which cannot be used to identify your facebook profile directly.</li>
+									   <li><b>Name</b> which is used to pre-fill the name field on listing forms and under "sellers name".</li>
+									   <li><b>Profile Picture</b> Which we show in the top corner when you are logged in as well as show it on your listings.</li>
+									   <li><b>Contact Link</b> You may provide a contact link, likley in the form a facebook profile link but maybe a mobile number or email address. These are not used by this website other than being displayed on your listing to logged in users.</li>									   
 									   <li><b>Any information in your listing</b> Any information you choose to include in your listing is stored but not used by this website other than being displayed.</li>
 								   </ul>
 							   </li>
